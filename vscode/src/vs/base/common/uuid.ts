9c955e88b6fcc1beec01a674e657236d34093e13
Sushi <mls.cryptofan@gmail.com>
2025-01-24 18:35:56 +0100

    Add DOCS

---
+++ vscode/src/vs/base/common/uuid.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+
+const _UUIDPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+export function isUUID(value: string): boolean {
+	return _UUIDPattern.test(value);
+}
+
+export const generateUuid: () => string = crypto.randomUUID.bind(crypto);
